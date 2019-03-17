6c04df6aea3fed7880d90c19e289d1fb25314ea2
Jens Lindberg <elusiveunit@gmail.com>
2019-03-17 09:55:52 +0100

    Add icons and meta stuff, change build folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin');
 
 const env = process.env.NODE_ENV;
 const isProd = env === 'production';
+const buildFolder = 'build';
 
 module.exports = {
   mode: env,
   devtool: isProd ? 'source-map' : 'inline-source-map',
   entry: './src/index.js',
   output: {
-    path: `${__dirname}/public`,
+    path: `${__dirname}/${buildFolder}`,
     publicPath: '/',
     filename: 'app.[contenthash].js',
   },
@@ -63,23 +65,32 @@ module.exports = {
     ],
   },
   plugins: [
-    isProd && new CleanWebpackPlugin(),
+    isProd && new CleanPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
     }),
-    new HtmlWebPackPlugin({
+    new HtmlPlugin({
       template: isProd
         ? '!!prerender-loader?string!./templates/index.html'
         : './templates/index.html',
       inject: 'head',
       minify: false,
     }),
-    new ScriptExtHtmlWebpackPlugin({
+    new ScriptExtHtmlPlugin({
       defaultAttribute: 'defer',
     }),
+    isProd &&
+      new CopyPlugin([
+        {
+          from: 'public/*',
+          transformPath(targetPath) {
+            return targetPath.replace(/public\/?/, '');
+          },
+        },
+      ]),
   ].filter((p) => Boolean(p)),
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: path.join(__dirname, `${buildFolder}/`),
     port: 3000,
   },
 };
