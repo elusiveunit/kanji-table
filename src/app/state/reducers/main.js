ed1154aaa306ac1c7ca57f7f48796f3df227b715
Jens Lindberg <elusiveunit@gmail.com>
2019-04-06 19:02:12 +0200

    Log actions on dev

--- src/app/state/reducers/main.js
+++ src/app/state/reducers/main.js
@@ -17,5 +17,29 @@ const reducers = {
 };
 
 export default function mainReducer(state, action) {
-  return mapObject(reducers, (reducer, key) => reducer(state[key], action));
+  const result = mapObject(reducers, (reducer, key) =>
+    reducer(state[key], action),
+  );
+
+  if (process.env.NODE_ENV === 'development') {
+    /* eslint-disable no-console */
+    const resultDiff = Object.keys(result).reduce(
+      (diff, key) =>
+        (state[key] === result[key]
+          ? diff
+          : {
+            ...diff,
+            [key]: result[key],
+          }),
+      {},
+    );
+    console.groupCollapsed(`action [${action.type}]`);
+    console.log('[ACTION]', action);
+    console.log('[BEFORE]', state);
+    console.log('[AFTER]', result);
+    console.log('[CHANGED]', resultDiff);
+    console.groupEnd();
+  }
+
+  return result;
 }
